d7d5f3b0e3f63b5d3e3d9467c88c09fbca890714
paul-uu <pwittschen@gmail.com>
2015-01-23 10:11:09 -0500

    Create high_scores.js

---
+++ high_scores.js
@@ -0,0 +1,14 @@
+{
+  "high_scores":[
+    {"name":"paul is dum","score":"232"},
+    {"name":"Kevin Spacey","score":"215"},
+    {"name":"vitamin-b12","score":"189"},
+    {"name":"filthy-casual","score":"181"},
+    {"name":"Mike Piazza","score":"176"},
+    {"name":"Frank Underwood","score":"168"},
+    {"name":"charizard","score":"168"},
+    {"name":"Han Solo","score":"161"},
+    {"name":"PIZZA!!!!!","score":"158"},
+    {"name":"Paul Krugman","score":"150"}
+  ]
+}
